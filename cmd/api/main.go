e9d0c724152db73a4616e180d7e562255528d36f
Sergey Rogozin <s.rogozin@vlig44.ru>
2025-08-15 15:24:53 +0300

    написали все middlewares

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -6,6 +6,7 @@ import (
 	"log"
 	"net/http"
 	"strings"
+	"time"
 
 	mw "github.com/Elren44/neural-go-rest/internal/api/middlewares"
 )
@@ -117,9 +118,11 @@ func main() {
 		MinVersion: tls.VersionTLS12,
 	}
 
+	rl := mw.NewRateLimiter(5, time.Minute)
+
 	server := &http.Server{
 		Addr:      port,
-		Handler:   mw.ResponseTimeMiddleware(mw.SecurityHeaders(mw.Cors(mux))),
+		Handler:   rl.Middleware(mw.Compression(mw.ResponseTimeMiddleware(mw.SecurityHeaders(mw.Cors(mux))))),
 		TLSConfig: tlsConfig,
 	}
 
--- internal/api/middlewares/compression.go
+++ internal/api/middlewares/compression.go
@@ -1,10 +1,44 @@
 package middlewares
 
-import "net/http"
+import (
+	"compress/gzip"
+	"fmt"
+	"net/http"
+	"strings"
+)
 
 func Compression(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 
+		if !strings.Contains(r.Header.Get("Accept-Encoding"), "gzip") {
+			// If the client does not support gzip, skip compression
+			next.ServeHTTP(w, r)
+			return
+		}
+
+		// Set the Content-Encoding header to gzip
+		w.Header().Set("Content-Encoding", "gzip")
+		gz := gzip.NewWriter(w)
+		defer gz.Close()
+
+		w = &gzipResponseWriter{
+			ResponseWriter: w,
+			Writer:         gz,
+		}
+
 		next.ServeHTTP(w, r)
+		fmt.Println("Response compressed with gzip for", r.URL.Path)
 	})
+}
+
+type gzipResponseWriter struct {
+	http.ResponseWriter
+	Writer *gzip.Writer
+}
+
+func (grw *gzipResponseWriter) Write(b []byte) (int, error) {
+	if grw.Writer == nil {
+		return grw.ResponseWriter.Write(b)
+	}
+	return grw.Writer.Write(b)
 }
